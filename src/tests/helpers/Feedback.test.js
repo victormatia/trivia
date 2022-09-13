5b1833c45f372c4b89e69b460af0e55c39aa2fb7
LUCAS <lucazeduardo3@gmail.com>
2022-09-13 20:32:01 -0300

    Resolve requisito 17

---
+++ src/tests/helpers/Feedback.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import App from "../../App";
+import Feedback from "../../pages/Feedback";
+import userEvent from "@testing-library/user-event";
+import { screen } from '@testing-library/react';
+import { renderWithRouterAndRedux } from './renderWithRouterAndRedux';
+
+
+describe("Testes da página Feedback", () => {
+
+    test('Testa se o nome do usuario aparece na tela', ()=>{
+        const {history}=renderWithRouterAndRedux(<App/>)
+        history.push('/feedback')
+
+        const playerName = screen.getByTestId('header-player-name')
+        expect(playerName).toBeInTheDocument()
+    })
+
+    test('Testa se o score do usuario aparece na tela', ()=>{
+        const {history}=renderWithRouterAndRedux(<App/>)
+        history.push('/feedback')
+
+        const playerScore = screen.getByTestId('header-score')
+        expect(playerScore).toBeInTheDocument()
+    })
+
+    test('Testa se o botão de Ranking aparece na tela', async ()=>{
+        const {history}=renderWithRouterAndRedux(<App/>)
+        history.push('/feedback')
+
+        const rannkingButton = screen.getByTestId('btn-ranking')
+        expect(rannkingButton).toBeInTheDocument()
+        userEvent.click(rannkingButton);
+        await expect(history.location.pathname).toBe('/ranking');
+    })
+
+    test('Testa se o botão de Play Again aparece na tela', ()=>{
+        const {history}=renderWithRouterAndRedux(<App/>)
+        history.push('/feedback')
+        
+        const btnPlayAgain = screen.getByTestId('btn-play-again')
+        expect(btnPlayAgain).toBeInTheDocument()
+        userEvent.click(btnPlayAgain)
+        expect(history.location.pathname).toBe('/')
+    })
+
+
+    test('Testa se a foto está na tela', ()=>{
+        renderWithRouterAndRedux(<Feedback />);
+        
+        const imagem = screen.getByTestId("header-profile-picture");
+        expect(imagem).toBeInTheDocument();
+      });
+})
